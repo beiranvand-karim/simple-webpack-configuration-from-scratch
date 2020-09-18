5448eb7c932d4d00e3c50ace618ac9a42bb44462
beiranvand-karim <beiranvand.karim@gmail.com>
2020-09-18 17:18:50 +0200

    Configure webpack from scratch for a simple project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from  'react-dom'
+// import '../styles/index.css'
+import '../styles/styles.scss'
+const App = () => {
+    return (
+        <h1>Hello world!</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
